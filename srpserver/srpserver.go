98c81b96cc6ee3049d93c0585a1123e398afd396
Jacob Saporito <jmsaporito@ucdavis.edu>
2013-11-18 23:08:01 -0800

    Generate a salt.

--- srpserver/srpserver.go
+++ srpserver/srpserver.go
@@ -1,14 +1,42 @@
 package srpserver
 
+import (
+    "crypto/rand"
+    "crypto/sha256"
+    "code.google.com/p/go.crypto/pbkdf2"
+    "fmt"
+    "io"
+)
+
 type Verifier struct {
-    salt int
-    key int
+    s []byte
+    v []byte
 }
 
-func CreateVerifier() Verifier {
-    return Verifier{0, 0}
+type ErrShortSalt struct {
+     n, slen int
 }
 
-func TestVerifier() {
+func (e ErrShortSalt) Error() string {
+     return fmt.Sprintf("Salt is too short. Expected length %d, got length %d.", e.slen, e.n)
+}
 
+func kdf(to_hash, salt []byte) []byte {
+    dk := pbkdf2.Key(to_hash, salt, 4096, 32, sha256.New)     
+
+    return dk
+}
+
+func CreateVerifier(p string, slen int) (Verifier, error) {
+    //Create a SRP verifier, given a password p, and the length of the desired salt.
+    salt := make([]byte, slen)
+    n, err := io.ReadFull(rand.Reader, salt)
+
+    if err != nil {
+        return Verifier{}, err
+    } else if n < slen {
+        return Verifier{}, ErrShortSalt{n, slen}
+    }
+
+    return Verifier{salt, []byte("test")}, nil
 }
