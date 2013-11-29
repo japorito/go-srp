cddff00a349484dddb6fe150ff44a37085b34ebe
Jacob Saporito <jmsaporito@ucdavis.edu>
2013-11-28 21:17:49 -0800

    Cleaned up API a bit. (Wiki is now out-of-date) Added functions to generate the first server side message (json-formatted, contains the salt and B).

--- srpserver/srpserver.go
+++ srpserver/srpserver.go
@@ -1,84 +1,185 @@
-// The go-srp/srpserver package contains server-side utilities necessary to 
+// The go-srp/srpserver package contains server-side utilities necessary to
 // implement SRP v. 6a, as described here: http://srp.stanford.edu/design.html
 // It aims to be a generalized implementation for use with various hash functions
 // and key/salt sizes.
 package srpserver
 
 import (
-    "crypto/rand"
-    "fmt"
-    "github.com/japorito/go-srp"
-    "io"
-    "math/big"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"github.com/japorito/go-srp"
+	"io"
+	"math/big"
 )
 
-type Verifier struct {
-    S []byte //salt
-    V []byte //verifier
+var gp srp.SRPGroupParameters
+var h func([]byte, []byte) big.Int
+var sgen func(uint) (big.Int, error)
+
+var Debug big.Int
+
+func SrpServer(srpgp srp.SRPGroupParameters, hash func([]byte, []byte) big.Int, salt_gen func(uint) (big.Int, error)) {
+	gp = srpgp
+	h = hash
+	sgen = salt_gen
 }
 
-type ErrShortSalt struct {
-     n, slen int
+func check_init() error {
+        if h == nil || sgen == nil || len(gp.G.Bytes()) == 0 || len(gp.N.Bytes()) == 0 {
+	        var err ErrorUninitializedSrpServer
+		return err
+	}
+
+	return nil
 }
 
-func (e ErrShortSalt) Error() string {
-     return fmt.Sprintf("Generated salt is was shorter than requested. Expected length %d, got length %d.", e.slen, e.n)
+type ErrorUninitializedSrpServer string
+
+func (e ErrorUninitializedSrpServer) Error() string {
+        return fmt.Sprintln("SRP Server improperly initialized. Please call SrpServer() with valid SRPGroupParameters, a hash function, and a salt generating function.", e)
 }
 
 // Creates an entirely random salt of length slen.
 // For use with Create, if you only need to specify a hash function.
-func RandomSalt(slen int) ([]byte, error) {
-    salt := make([]byte, slen)
-    n, err := io.ReadFull(rand.Reader, salt)
+func RandomBytes(slen uint) (big.Int, error) {
+	s := make([]byte, slen)
+	n, err := io.ReadFull(rand.Reader, s)
+	var salt big.Int
+	salt.SetBytes(s)
+	
+	//check for errors, make sure salt is of the desired length.
+	if err != nil {
+		return salt, err
+	} else if uint(n) < slen {
+		return salt, ErrorShortSalt{n, slen}
+	}
 
-    //check for errors, make sure salt is of the desired length.
-    if err != nil {
-        return salt, err
-    } else if n < slen {
-        return salt, ErrShortSalt{n, slen}
-    }
-
-    return salt, nil
+	return salt, nil
 }
 
-//Initializes new verifier by picking a salt of the desired length slen, and running
-//the Hash function H which is passed to it.
-func (v *Verifier) Create(p string, slen int, H func([]byte, []byte) []byte, SGen func(int) ([]byte, error)) (*Verifier, error) {
-    // Create a SRP verifier, given a password p, and the length of the desired salt,
-    // a hash function, and a salt generating function. This is the DIY version of
-    // Verifier.New()
-    //Create random salt
-    var err error
-    v.S, err = SGen(slen)
-
-    //check for errors, make sure salt is of the desired length.
-    if err != nil {
-        return &Verifier{}, err
-    }
-
-    //run hash function on password and salt
-    dk := H([]byte(p), v.S)
-    var x big.Int
-    x.SetBytes(dk) //convert []byte to big.Int
-
-    //get group parameters, to use hashing function/create verifier
-    gp, err := srp.GetGroupParameters(1024)
-
-    //check for errors, exit if group parameters of that size not found
-    if err != nil {
-        return &Verifier{}, err
-    }
-
-    //Prepare variables for big.Int exponentiation call
-    var g big.Int
-    g.SetInt64(int64(gp.G))
-
-    //create verifier v with hash and g (g**x % N)
-    v.V = x.Exp(&g, &x, &gp.N).Bytes()
-
-    return v, nil
+type ErrorShortSalt struct {
+	n    int
+	slen uint
 }
 
-func (v *Verifier) New(p string) (*Verifier, error) {
-     return v.Create(p, 32, srp.H, RandomSalt)
+func (e ErrorShortSalt) Error() string {
+	return fmt.Sprintf("Generated salt is was shorter than requested. Expected length %d, got length %d.", e.slen, e.n)
+}
+
+type Verifier struct {
+	I        string
+	Salt     big.Int //salt
+	Verifier big.Int //verifier
+}
+
+// Create an SRP verifier, given a password p, and the length of the desired salt,
+// a hash function, and a salt generating function. This is the DIY version of
+// Verifier.New()
+// Note: does NOT take a function that generates a hash.Hash, because this would
+// preclude doing things like pbkdf2, which takes the password and salt and deals
+// with them separately. The function handed to this should be a wrapper function
+// that takes the password and salt as its 1st and 2nd argument respectively.
+func (v *Verifier) New(user, p string, slen uint) (*Verifier, error) {
+	//Create random salt
+	var err error
+
+	v.I = user
+	v.Salt, err = sgen(slen)
+
+	//check for errors, make sure salt is of the desired length.
+	if err != nil {
+		return &Verifier{}, err
+	}
+
+	//run hash function on password and salt
+	x := h([]byte(p), v.Salt.Bytes())
+
+	//create verifier v with hash and g (g**x % N)
+	v.Verifier.Exp(&gp.G, &x, &gp.N)
+
+	return v, nil
+}
+
+type SRPSession struct {
+	i                string
+	s, v    	 big.Int
+	b		 big.Int
+	biga, bigb       big.Int
+	session_key      big.Int
+}
+
+func (s *SRPSession) ReadChallenge(jsonIA string) error {
+        err := check_init()
+
+	return err
+}
+
+func (s *SRPSession) ChallengeResponse(v Verifier) (string, error) {
+        err := check_init()
+
+	var message challenge_response
+	message.new(v, s)
+
+	//Initialize SRPSession fields. s.littleb is set in challenge_response.new
+	//s.I should be set in ReadChallenge
+	s.s = v.Salt
+	s.v = v.Verifier
+
+	output, err := json.MarshalIndent(message, "", "    ")
+
+	if err != nil {
+	        return "", err
+	}
+
+	return string(output), nil
+}
+
+type ErrorWrongB struct {
+        expectedLen, gotLen int
+}
+
+func (e ErrorWrongB) Error() string {
+     return fmt.Sprintf("Read B value different than expected. Expected value of length %v bytes, read %v bytes", e.expectedLen, e.gotLen)
+}
+
+type challenge_response struct {
+	Salt string
+	B string
+}
+
+// Initialize a challenge_response object, which contains the data that is
+// sent from the server to the client.
+func (msb *challenge_response) new(v Verifier, sess *SRPSession) (*challenge_response, error) {
+	var B big.Int
+	Ng := make([]byte, len(gp.N.Bytes())+len(gp.G.Bytes())) 
+	// make the buffer big enough for N and g
+
+	//calculate kv
+	copy(Ng, gp.N.Bytes())
+	copy(Ng[len(gp.N.Bytes()):], gp.G.Bytes())
+	kv := h(Ng, make([]byte, 0))
+	kv.Mul(&kv, &v.Verifier)
+
+	msb.Salt = fmt.Sprintf("%X", v.Salt.Bytes())
+
+	//generate random b
+	Debug, err := RandomBytes(64)
+	sess.b = Debug
+
+	//check for errors, make sure salt is of the desired length.
+	if err != nil {
+		return &challenge_response{}, err
+	}
+
+	//calculate B = kv+g^b
+	B.Exp(&gp.G, &sess.b, &gp.N)
+	B.Add(&B, &kv)
+
+	//store B for later calculations.
+	sess.bigb = B
+
+	msb.B = fmt.Sprintf("%X", B.Bytes())
+
+	return msb, nil
 }
--- srpserver/srpserver_test.go
+++ srpserver/srpserver_test.go
@@ -1,94 +1,179 @@
 package srpserver
 
 import (
-    "crypto/sha1"
-    "math/big"
-    "testing"
+	"crypto/sha1"
+	"github.com/japorito/go-srp"
+	"math/big"
+	"testing"
 )
 
 var v Verifier
+var s SRPSession
 
 // Gives a non-random salt for the purpose of testing.
-func sgen(slen int) ([]byte, error) {
-    var salt big.Int
-    salt.SetString("BEB25379D1A8581EB5A727673A2441EE", 16)
-    return salt.Bytes(), nil
+func testgen(slen uint) (big.Int, error) {
+	var salt big.Int
+	salt.SetString("BEB25379D1A8581EB5A727673A2441EE", 16)
+	return salt, nil
 }
 
 // Implements the hash function described in RFC 5054
 // so that we can use their test vector to verify our code
-func h(to_hash, salt []byte) []byte {
-    var hash []byte
-    outerhash := sha1.New()
-    innerhash := sha1.New()
-    
-    innerhash.Write(to_hash)
-    salt = innerhash.Sum(salt)
-    outerhash.Write(salt)
+func testh(to_hash, salt []byte) big.Int {
+	var hash []byte
+	var output big.Int
 
-    return outerhash.Sum(hash)
+	outerhash := sha1.New()
+	innerhash := sha1.New()
+
+	innerhash.Write(to_hash)
+	salt = innerhash.Sum(salt)
+	outerhash.Write(salt)
+
+	hash = outerhash.Sum(hash)
+	output.SetBytes(hash)
+
+	return output
 }
 
-// Tests v.Create() with a simple hash function and a given salt
+func TestCheckInit(t *testing.T) {
+        err := check_init()
+
+	if err == nil {
+	        t.Error("Error: failed to find uninitialized SrpServer.")
+	} else {
+		t.Log("check_init() correctly found uninitialized SrpServer.")
+	}
+
+     	testgp, err := srp.GetGroupParameters(1024)
+
+	if err != nil {
+	        t.Error("Error: ", err)
+	}
+
+        SrpServer(testgp, testh, testgen)
+	err = check_init()
+
+	if err != nil {
+	        t.Error(err)
+	} else {
+		t.Log("check_init() correctly found SrpServer to be initialized.")
+	}
+}
+
+// Tests v.New() with a simple hash function and a given salt
 // checks output with test vector from appendix of RFC 5054
-func TestCreate(t *testing.T) {
-    _, err := v.Create("alice:password123", 32, h, sgen)
-    if err == nil {
-       t.Logf("Verifier is %X.\nSalt is %X.\n", v.V, v.S)
-    } else {
-       t.Error("Error: ", err)
-    }
+func TestSimpleHashSalt(t *testing.T) {
+     	testgp, err := srp.GetGroupParameters(1024)
 
-    var correctv, corrects big.Int
-    correctv.SetString("7E273DE8696FFC4F4E337D05B4B375BEB0DDE1569E8FA00A9886D8129BADA1F1822223CA1A605B530E379BA4729FDC59F105B4787E5186F5C671085A1447B52A48CF1970B4FB6F8400BBF4CEBFBB168152E08AB5EA53D15C1AFF87B2B9DA6E04E058AD51CC72BFC9033B564E26480D78E955A5E29E7AB245DB2BE315E2099AFB", 16)
-    corrects.SetString("BEB25379D1A8581EB5A727673A2441EE", 16)
-    
-    var fail bool = false
+	if err != nil {
+	        t.Error("Error: ", err)
+	}
 
-    salt := corrects.Bytes()
-    for i := range salt {
-        if v.S[i] != salt[i] {
-	    fail = true
-        }
-    } 
+        SrpServer(testgp, testh, testgen)
+	_, err = v.New("alice", "alice:password123", 32)
+	if err == nil {
+		t.Logf("Verifier is %X.\nSalt is %X.\n", v.Verifier.Bytes(), v.Salt.Bytes())
+	} else {
+		t.Error("Error: ", err)
+	}
 
-    if fail {
-        t.Error("Error: recieved incorrect salt.")
-	fail = false
-    }
+	var correctv, corrects big.Int
+	correctv.SetString("7E273DE8696FFC4F4E337D05B4B375BEB0DDE1569E8FA00A9886D8129BADA1F1822223CA1A605B530E379BA4729FDC59F105B4787E5186F5C671085A1447B52A48CF1970B4FB6F8400BBF4CEBFBB168152E08AB5EA53D15C1AFF87B2B9DA6E04E058AD51CC72BFC9033B564E26480D78E955A5E29E7AB245DB2BE315E2099AFB", 16)
+	corrects.SetString("BEB25379D1A8581EB5A727673A2441EE", 16)
 
-    verifier := correctv.Bytes()
-    for i := range verifier {
-        if v.V[i] != verifier[i] {
-	    fail = true
-        }
-    }
+	if len(corrects.Bytes()) != len(v.Salt.Bytes()) {
+	        t.Error("Error: recieved incorrect salt.")
+	}
 
-    if fail {
-        t.Errorf("Error: Incorrect verifier.\nExpected: %X\nGot: %X", verifier, v.V)
-    }
+	var fail bool = false
+
+	correct_salt := corrects.Bytes()
+	got_salt := v.Salt.Bytes()
+	for i := range correct_salt {
+		if got_salt[i] != correct_salt[i] {
+			fail = true
+		}
+	}
+
+	if fail {
+		t.Error("Error: recieved incorrect salt.")
+		fail = false
+	}
+
+	if len(correctv.Bytes()) != len(v.Verifier.Bytes()) {
+	        t.Error("Error: recieved incorrect verifier.")
+	}
+
+	correct_verifier := correctv.Bytes()
+	got_verifier := v.Verifier.Bytes()
+	for i := range correct_verifier {
+		if got_verifier[i] != correct_verifier[i] {
+			fail = true
+		}
+	}
+
+	if fail {
+		t.Errorf("Error: Incorrect verifier.\nExpected: %X\nGot: %X", correct_verifier, got_verifier)
+	}
+
+	if v.I != "alice" {
+	        t.Errorf("Error: username should be set to alice. Got %v", v.I)
+	}
 }
 
-
-// Runs RandomSalt. Errors if err is non-nil
-func TestRandomSalt(t *testing.T) {
-    _, err := RandomSalt(10)
-    if err != nil {
-        t.Error(err)
-    }
+// Runs RandomBytes. Errors if err is non-nil
+func TestRandomBytes(t *testing.T) {
+	n, err := RandomBytes(64)
+	if err != nil {
+		t.Error(err)
+	} else if len(n.Bytes()) != 64 {
+	        t.Error("Error: RandomBytes gave an incorrect length.")
+	}
+	
+	t.Logf("Salt: %X", n.Bytes())
 }
 
-func TestNew(t *testing.T) {
-     _, err := v.New("password")
-     if err != nil {
-         t.Error(err)
-     } else if len(v.S) < 32 {
-         t.Error(ErrShortSalt{len(v.S), 32})
-     } else if len(v.V) == 0 {
-         t.Error("Error: empty verifier returned.")
-     }
+func TestStandardHashSalt(t *testing.T) {
+        var tmpv Verifier
+        testgp, err := srp.GetGroupParameters(4096)
 
-     t.Log("Verifier successfully created via v.New()")
-     t.Log("Salt: ", v.S)
-     t.Log("Verifier: ", v.V)
+	if err != nil {
+	        t.Error("Error: ", err)
+	}
+
+        SrpServer(testgp, srp.H, RandomBytes)
+	_, err = tmpv.New("username", "password", 32)
+	if err != nil {
+		t.Error(err)
+	} else if len(tmpv.Salt.Bytes()) != 32 {
+		t.Error(ErrorShortSalt{len(tmpv.Salt.Bytes()), 32})
+	} else if len(tmpv.Verifier.Bytes()) == 0 {
+		t.Error("Error: empty verifier returned.")
+	} else if tmpv.I != "username" {
+	        t.Errorf("Error: username should be set to \"username\". Got %v", tmpv.I)
+	}
+
+	t.Log("Verifier successfully created via v.New()")
+	t.Logf("Salt: %X", tmpv.Salt.Bytes())
+	t.Logf("Verifier: %X", tmpv.Verifier.Bytes())
+	t.Logf("littleb: %X", Debug.Bytes())
+}
+
+func TestChallengeResponse(t *testing.T) {
+        testgp, err := srp.GetGroupParameters(1024)
+
+	if err != nil {
+	        t.Error("Error: ", err)
+	}
+
+        SrpServer(testgp, testh, testgen)
+
+	cr, err := s.ChallengeResponse(v)
+
+	if err != nil {
+	        t.Error("Error: ", err)
+	} else {
+	        t.Log("JSON encoded challenge response containing s and B: ", cr)
+	}
 }
